6e55ea029aa02cc3f9075193ec9978f69c72e698
Patrick da Silva <patrickeeep12@gmail.com>
2017-12-31 12:45:32 -0300

    Criando o routing module

--- apps/angular2/01-cadastro-contatos/app/app.module.ts
+++ apps/angular2/01-cadastro-contatos/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { ContatosModule } from './contatos/contatos.module';
 
 @NgModule({
     imports: [
-        BrowserModule,
+        AppRoutingModule,
+        BrowserModule,        
         ContatosModule
     ],
     declarations: [AppComponent],
