740c04ca9220cb6fa7c1901797e3296eb0d08337
Patrick da Silva <patrickeeep12@gmail.com>
2017-12-31 06:22:24 -0300

    Adicionando Bootstrap ao projeto

--- apps/angular2/01-cadastro-contatos/app/app.component.ts
+++ apps/angular2/01-cadastro-contatos/app/app.component.ts
@@ -3,6 +3,10 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App, [Sou demais]</h1>'
+    template: `
+        <h1>My First Angular 2 App, [Sou demais]</h1>
+        
+        <button class="btn btn-success" >Hello Bootstrap</button>
+    `
 })
 export class AppComponent {}
