fe0fc7dd1c3580cf23d20c9fab937aa76ac902c2
Patrick da Silva <patrickeeep12@gmail.com>
2017-12-31 13:32:05 -0300

    Barra de navegação e botões

--- apps/angular2/01-cadastro-contatos/app/app.component.ts
+++ apps/angular2/01-cadastro-contatos/app/app.component.ts
@@ -2,11 +2,8 @@ import { Component } from '@angular/core';
 
 
 @Component({
+    moduleId: module.id,
     selector: 'my-app',
-    template: `
-        <h1>My First Angular 2 App</h1>
-        
-        <router-outlet></router-outlet>
-    `
+    templateUrl: 'app.component.html'
 })
 export class AppComponent {}
