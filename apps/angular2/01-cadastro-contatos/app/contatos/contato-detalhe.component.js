a71ee9bc5174ca3937588a3ebbe7fb9f7b39d001
Patrick da Silva <patrickeeep12@gmail.com>
2018-01-21 20:27:02 -0300

    Mudanças de estados e validade com ngModel

--- apps/angular2/01-cadastro-contatos/app/contatos/contato-detalhe.component.js
+++ apps/angular2/01-cadastro-contatos/app/contatos/contato-detalhe.component.js
@@ -42,7 +42,15 @@ ContatoDetalheComponent = __decorate([
     core_1.Component({
         moduleId: module.id,
         selector: "contato-detalhe",
-        templateUrl: "contato-detalhe.component.html"
+        templateUrl: "contato-detalhe.component.html",
+        styles: [`
+      .ng-valid[required] {
+          border: 3px solid green;
+      }
+      .ng-invalid:not(form) {
+          border: 3px solid tomato;
+      }
+  `]
     }),
     __metadata("design:paramtypes", [contato_service_1.ContatoService,
         router_1.ActivatedRoute,
--- apps/angular2/01-cadastro-contatos/app/contatos/contato-detalhe.component.ts
+++ apps/angular2/01-cadastro-contatos/app/contatos/contato-detalhe.component.ts
@@ -8,7 +8,15 @@ import { ContatoService } from "./contato.service";
 @Component({
   moduleId: module.id,
   selector: "contato-detalhe",
-  templateUrl: "contato-detalhe.component.html"
+  templateUrl: "contato-detalhe.component.html",
+  styles: [`
+      .ng-valid[required] {
+          border: 3px solid green;
+      }
+      .ng-invalid:not(form) {
+          border: 3px solid tomato;
+      }
+  `]
 })
 export class ContatoDetalheComponent implements OnInit {
   // one-way database, porque estamos pegando o atributo da classe e exibindo no html
